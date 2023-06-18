3f31685097dcaae026e175179d5fb839d5cd43b3
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-18 20:55:08 +0200

    Initialize project and structure

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const handlebars = require('express-handlebars')
+
+const routes = require('./routes');
+
+const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs')
+
+app.use(express.static('static'));
+app.use(express.urlencoded({extended: false}))
+
+
+app.use(routes)
+
+app.listen(3000, console.log('Server is listening on port 3000...'));
