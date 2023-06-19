7cf284eca76f9332105bc5d77cb89b84681b10bd
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-19 11:25:19 +0200

    Setup register with hash password

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); //Bcrypt hash password
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'Email is required'],
+        unique: true,
     },
     password: {
         type: String,
         required: [true, 'Password is required'],
     }
+});
+
+//Password validation
+userSchema.virtual('repeatPassword')
+.set(function(value){
+    if(this.password !== value){
+        throw new Error('Password missmatch!')
+    }
+})
+
+//Bcrypt hash password
+userSchema.pre('save', async function(){
+const hash = await bcrypt.hash(this.password, 10);
+
+this.password = hash
 })
 
 const User = mongoose.model('User', userSchema);
