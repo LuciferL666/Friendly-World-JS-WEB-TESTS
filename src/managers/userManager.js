8ec4c613b580c3fa53ac80ba6173a50779060e42
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-20 01:46:43 +0200

    Structure for guest and user

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,20 +1,39 @@
 //THIS WILL SAVE IT IN DB
-const User = require('../models/User');
+const bcrypt = require('bcrypt')
 
-exports.login = (email, password) => {
+const jwt = require('../lib/jwt');
+const User = require("../models/User");
+const { SECRET } = require('../config/config')
 
+exports.login = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
+  const isValid = await bcrypt.compare(password, user.password);
+  if(!isValid){
+    throw new Error("Invalid email or password");
+  }
+
+  const payload = {
+    _id: user._id,
+    email: user.email,
+    //password: user.password
+  }
+  const token = await jwt.sign(payload, SECRET, {expiresIn: '2d'});
+  
+  return token;
 };
-//cheking for unique user 
-exports.register = async (userData) => { //if I can't make it use this exports.register = (userData) => User.create(userData) only
-    const user = await User.findOne({email: userData.email});
-    if(user){
-throw new Error('Username already exists');
-    }
+//cheking for unique user
+exports.register = async (userData) => {
+  //if I can't make it use this exports.register = (userData) => User.create(userData) only
+  const user = await User.findOne({ email: userData.email });
+  if (user) {
+    throw new Error("Username already exists");
+  }
 
-    return User.create(userData);
-}
+  return User.create(userData);
+};
 
-
-
-//exports.logout = () => 
-
+//exports.logout = () =>
