29dee677574f00f747669c581d8e068854299764
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-19 11:25:43 +0200

    Check for unique user

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,13 +1,20 @@
+//THIS WILL SAVE IT IN DB
 const User = require('../models/User');
 
 exports.login = (email, password) => {
 
 };
+//cheking for unique user 
+exports.register = async (userData) => { //if I can't make it use this exports.register = (userData) => User.create(userData) only
+    const user = await User.findOne({email: userData.email});
+    if(user){
+throw new Error('Username already exists');
+    }
 
-exports.register = (userData) => {
-
+    return User.create(userData);
 }
 
-exports.logout = () => {
 
-};
+
+//exports.logout = () => 
+
