ea74f2b3d4dd0d8767008b6d93121dae909b3bc3
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-19 10:13:25 +0200

    Add login and register post actions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,8 +6,23 @@ router.get('/login', (req, res)=>{
 res.render('users/login')
 });
 
+router.post('/login', async(req, res) =>{
+    const {email, password} = req.body
+    await userManager.login(email, password);
+
+    res.send('Logged in')
+});
+
 router.get('/register', (req, res)=>{
     res.render('users/register')
 })
+
+router.post('/register', async (req, res) =>{
+    const {email, password, repeatPassword} = req.body;
+await userManager.register({email, password, repeatPassword});
+
+res.send('registered')
+});
+
 
 module.exports = router;
--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,10 +1,10 @@
 const User = require('../models/User');
 
-exports.login = () => {
+exports.login = (email, password) => {
 
 };
 
-exports.register = () => {
+exports.register = (userData) => {
 
 }
 
