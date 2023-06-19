d6357d86cfea09741ea7766e66ec3fb8c93ef5c5
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-20 01:45:16 +0200

    create import token key and secret

---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+exports.SECRET = 'b5db034f-7fe5-42bc-8018-1def93c43b2f';
+
+exports.TOKEN_KEY = 'token';
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,28 +1,37 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userManager = require('../managers/userManager')
+const userManager = require('../managers/userManager');
+const { TOKEN_KEY } =  require('../config/config')
 
-router.get('/login', (req, res)=>{
-res.render('users/login')
+router.get("/login", (req, res) => {
+  res.render("users/login");
 });
 
-router.post('/login', async(req, res) =>{
-    const {email, password} = req.body
-    await userManager.login(email, password);
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    res.send('Logged in')
+  const token = await userManager.login(email, password);
+
+  res.cookie(TOKEN_KEY, token);
+
+  res.redirect("/");
 });
 
-router.get('/register', (req, res)=>{
-    res.render('users/register')
-})
-
-router.post('/register', async (req, res) =>{
-    const {email, password, repeatPassword} = req.body;
-await userManager.register({email, password, repeatPassword});
-
-res.send('registered')
+router.get("/register", (req, res) => {
+  res.render("users/register");
 });
 
+router.post("/register", async (req, res) => {
+  const { email, password, repeatPassword } = req.body;
+  await userManager.register({ email, password, repeatPassword });
+
+  res.redirect('/users/login');
+});
+
+router.get('/logout', (req, res)=>{
+res.clearCookie('token');
+
+res.redirect('/')
+});
 
 module.exports = router;
