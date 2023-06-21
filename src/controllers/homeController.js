e8567de91f875bef470efe871287f5e9cc1ba11d
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-21 23:10:28 +0200

    Setup dashboard page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,12 +2,20 @@ const router = require('express').Router();
 
 router.get('/', (req, res)=>{
 //console.log(req.user);
-    res.render('home')
+    res.render('home');
+});
+
+router.get('/dashboard', (req, res)=>{
+    res.render('dashboard');
+});
+
+router.get('/search', (req, res)=>{
+    res.render('search');
 });
 
 router.get('/404', (req, res)=>{
-    res.render('404')
-})
+    res.render('404');
+});
 
 
 module.exports = router
