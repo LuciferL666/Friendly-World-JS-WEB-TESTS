9a1a7cb97db02f00d070be92c2c73620ff0dc0f1
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-22 23:40:53 +0200

    Update animal model

--- src/models/Animal.js
+++ src/models/Animal.js
@@ -3,31 +3,31 @@ const mongoose = require("mongoose");
 const animalSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Name is required']
   },
   years: {
     type: Number,
-    required: true,
+    required: [true, 'Years is required']
   },
   kind: {
     type: String,
-    required: true,
+    required: [true, 'Kind is required']
   },
   image: {
     type: String,
-    required: true,
+    required: [true, 'ImageUrl is required']
   },
   need: {
     type: String,
-    required: true,
+    required: [true, 'Need is required']
   },
   location: {
     type: String,
-    required: true,
+    required: [true, 'Location is required']
   },
   description: {
     type: String,
-    required: true,
+    required: [true, 'Description is required']
   },
   // • donations – an array of objects containing the user's ID
    owner: {
@@ -35,6 +35,10 @@ const animalSchema = new mongoose.Schema({
     ref: 'User',
    },
 });
+
+const Animal = mongoose.model('Animal', animalSchema);
+
+module.exports = Animal;
 
 
 // 3chasa i 27 min
