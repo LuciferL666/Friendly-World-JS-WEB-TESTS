7ee591bcaf7b05d0f9b81e480df2f9acf192e9a9
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-22 23:40:16 +0200

    Create post router for animal creation

--- src/controllers/animalController.js
+++ src/controllers/animalController.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
+const animalManager = require('../managers/animalManager')
+const { getErrorMessage } =  require('../utils/errorHelpers')
+
 router.get('/create', (req, res) =>{
     res.render('animals/create')
+});
+
+router.post('/create', async (req, res)=>{
+const {animalData} = req.body;
+try{
+    await animalManager.create(animalData)
+
+    res.redirect('/dashboard')
+}catch(err) {
+res.render('animals/create', {error: getErrorMessage(err)})
+}
 });
 
 module.exports = router;
