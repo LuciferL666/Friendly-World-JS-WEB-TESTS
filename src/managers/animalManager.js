94ce0a2ace58aa4f787f54ffb7221f3eda0a0928
PeykoGeorgiev <peyko92gm@gmail.com>
2023-06-26 22:58:23 +0200

    setup animal Id for creator be able to go on details

--- src/managers/animalManager.js
+++ src/managers/animalManager.js
@@ -1,5 +1,7 @@
 const Animal = require('../models/Animal')
 
-exports.getAll = () => Animal.find()
+exports.getAll = () => Animal.find().populate('owner')
+
+exports.getOne = (animalId) => Animal.findById(animalId).populate('owner')
 
 exports.create = (animalData) => Animal.create(animalData);
